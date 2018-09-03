67d30172a0fecdcc9a75ae29acfa729e91004617
Rachael Codron <rachaelcodron@Rachaels-MacBook-Pro.local>
2018-09-02 19:22:46 -0700

    fixed isRemoval and onClick

--- jammming/src/components/Track/Track.js
+++ jammming/src/components/Track/Track.js
@@ -4,7 +4,7 @@ import './Track.css';
 class extends Track React.Component {
   constructor(props) {
     super(props);
-    
+
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
   }
@@ -14,8 +14,8 @@ class extends Track React.Component {
   removeTrack() {
     this.props.onRemove(this.props.track); //#53
   }
-  renderAction() {           //not sure about this.. #27
-    return (isRemoval ? "-" : "+");
+  renderAction() {
+    return (this.props.isRemoval ? "-" : "+");
   }
   render() {
     return (
@@ -24,20 +24,10 @@ class extends Track React.Component {
         <h3>{this.props.track.name}</h3>
         <p>{this.props.track.artist} | {this.props.track.album}</p>
       </div>
-      <a className="Track-action">{this.renderAction}</a> //not sure if uses a .this?
+      <a className="Track-action" onClick={this.props.isRemoval ? this.removeTrack : this.addTrack}>{this.renderAction()}</a>
     </div>
   )
   }
 }
 
 export default Track;
-
-
-
-
-{
-  if (isRemoval) {
-    return <a onClick ={this.removeTrack}>-</> //#55
-  } else {
-    return <a onClick={this.addTrack}>+</> //#47
-}
--- jammming/src/components/TrackList/TrackList.js
+++ jammming/src/components/TrackList/TrackList.js
@@ -7,7 +7,7 @@ class TrackList extends React.Component {
     return (
     <div className="TrackList">
     {this.props.tracks.map(track) => {
-      return <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={true}/> //the onRemove and isRemoval..#52
+      return <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval}/> //the onRemove #52
     }}
     </div>
   )
