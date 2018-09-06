821653c9ad44fcc19e94453b368cf856d3456350
Rachael Codron <rachaelcodron@Rachaels-MacBook-Pro.local>
2018-09-06 16:05:32 -0700

    better spotify.js

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -43,6 +43,7 @@ const Spotify = {
 },
 
 
+/*
 //#89-94
   savePlaylist(playlistName, trackURIs) {
     if (playlistName && trackURIs) {
@@ -77,5 +78,38 @@ const Spotify = {
   return;
   }
 },
+
+export default Spotify;
+*/
+
+savePlaylist(name, trackUris) {
+    if (!name || !trackUris.length) {
+      return;
+    }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    let userId;
+
+    return fetch('https://api.spotify.com/v1/me', {headers: headers}
+    ).then(response => response.json()
+    ).then(jsonResponse => {
+      userId = jsonResponse.id;
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+        headers: headers,
+        method: 'POST',
+        body: JSON.stringify({name: name})
+      }).then(response => response.json()
+      ).then(jsonResponse => {
+        const playlistId = jsonResponse.id;
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+          headers: headers,
+          method: 'POST',
+          body: JSON.stringify({uris: trackUris})
+        });
+      });
+    });
+  }
+};
 
 export default Spotify;
